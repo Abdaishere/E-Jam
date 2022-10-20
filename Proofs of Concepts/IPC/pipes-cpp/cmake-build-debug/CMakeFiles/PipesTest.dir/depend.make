36b3d7ad1253cd34c50c8b900ea0dd3209e5ccb2
MohamedElHagry <mohamedelhagery@gmail.com>
2022-10-20 02:02:05 +0200

    minor changes in benchmarking

---
+++ Proofs of Concepts/IPC/pipes-cpp/cmake-build-debug/CMakeFiles/PipesTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PipesTest.
+# This may be replaced when dependencies are built.
---
+++ Proofs of Concepts/IPC/sockets-cpp/cmake-build-debug/CMakeFiles/SocketsTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SocketsTest.dir/client.cpp.o"
+  "CMakeFiles/SocketsTest.dir/client.cpp.o.d"
+  "CMakeFiles/SocketsTest.dir/server.cpp.o"
+  "CMakeFiles/SocketsTest.dir/server.cpp.o.d"
+  "SocketsTest"
+  "SocketsTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SocketsTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
