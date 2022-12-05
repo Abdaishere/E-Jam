03e7a90f537c01a777f685900ebac80954297388
Khaled-Waled <khaledw.hamed001@gmail.com>
2022-12-05 22:12:45 +0200

    [Merge] RNG branch

---
+++ Prototype/Generator/RNG.cpp
@@ -0,0 +1,18 @@
+//
+// Created by mohamedelhagry on 12/5/22.
+//
+
+#include "RNG.h"
+
+
+//unit testing for the rng
+/*int main()
+{
+    RNG rng;
+    for(int i=0; i<14; i++)
+    {
+        printf("%d ", rng.gen());
+        printf("%llu\n", rng.getR());
+    }
+}
+*/
