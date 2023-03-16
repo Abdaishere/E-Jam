30a6b19e844795b6255edfbe6d7590a082aa6925
mohamedelhagery <mohamedelhagery@gmail.com>
2023-03-16 04:41:34 +0200

    [feature] B2B and burst generation

--- Prototype/Generator/main.cpp
+++ Prototype/Generator/main.cpp
@@ -14,6 +14,7 @@ typedef unsigned long long ull;
 //TODO standarize the units
 
 //thread function to send the packets
+/// \param deprecated Should be deprecated because of the more specific controlled sending functions below
 void sendingFunction(std::shared_ptr<PacketCreator> pc)
 {
     while(true)
@@ -29,16 +30,18 @@ void sendingFunction(std::shared_ptr<PacketCreator> pc)
 
 /// \param duration time of sending in milliseconds (10^-3) seconds
 /// \details send a packets back to back for a certain duration
-void sendTimeBasedB2B(std::shared_ptr<PacketCreator>& pc, ull duration, ull IFG = 0)
+void sendTimeBasedB2B(std::shared_ptr<PacketCreator> pc, ull duration, ull IFG = 0)
 {
+    using namespace std::chrono;
+
     if(duration <= 0) return;
-    time_t beginTime = time(NULL);
-    time_t endTime = time(NULL);
-    while ((endTime - beginTime) <= duration)
+    time_point<steady_clock> beginTime = steady_clock::now();
+    time_point<steady_clock> endTime = steady_clock::now();
+    while (duration_cast<milliseconds>(endTime- beginTime).count() <= duration)
     {
         pc->sendHead();
-        endTime = time(NULL);
-        std::this_thread::sleep_for(std::chrono::milliseconds(IFG));
+        endTime = steady_clock::now();
+        std::this_thread::sleep_for(milliseconds(IFG));
     }
 }
 
@@ -46,43 +49,46 @@ void sendTimeBasedB2B(std::shared_ptr<PacketCreator>& pc, ull duration, ull IFG 
 /// \param delay time between bursts in milliseconds (10^-3) seconds
 /// \param burstSize number of packets per burst
 /// \details bursts of length burstSize with inter-burst gap delay for a certain duration
-void sendTimeBasedBurst(std::shared_ptr<PacketCreator>& pc, ull duration, ull delay, int burstSize, ull IFG = 0)
+void sendTimeBasedBurst(std::shared_ptr<PacketCreator> pc, ull duration, ull delay, int burstSize, ull IFG = 0)
 {
+    using namespace std::chrono;
     if(duration <= 0) return;
-    time_t beginTime = time(NULL);
-    time_t endTime = time(NULL);
+    time_point<steady_clock> beginTime = steady_clock::now();
+    time_point<steady_clock> endTime = steady_clock::now();
 
-    while ((endTime - beginTime) <= duration)
+    while (duration_cast<milliseconds>(endTime- beginTime).count() <= duration)
     {
         int remaining = burstSize;
         while(remaining--)
         {
             pc->sendHead();
-            std::this_thread::sleep_for(std::chrono::milliseconds(IFG));
+            std::this_thread::sleep_for(milliseconds(IFG));
         }
 
         //duration between bursts
-        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-        endTime = time(NULL);
+        std::this_thread::sleep_for(milliseconds(delay));
+        endTime = steady_clock::now();
     }
 }
 
 /// \details send a specified number of packets back to back
-void sendB2B(std::shared_ptr<PacketCreator>& pc, int packetsToSend, ull IFG = 0)
+void sendB2B(std::shared_ptr<PacketCreator> pc, int packetsToSend, ull IFG = 0)
 {
+    using namespace std::chrono;
     if(packetsToSend <= 0) return; 
     while(packetsToSend--)
     {
         pc->sendHead();
-        std::this_thread::sleep_for(std::chrono::milliseconds(IFG));
+        std::this_thread::sleep_for(milliseconds(IFG));
     }
 }
 
 
 /// \param delay in milliseconds
 /// \details send a specified number of packets in bursts of size burst
-void sendBurst(std::shared_ptr<PacketCreator>& pc, ull delay, int packetsToSend, int burst, ull IFG = 0)
+void sendBurst(std::shared_ptr<PacketCreator> pc, int packetsToSend, ull delay, int burst, ull IFG = 0)
 {
+    using namespace std::chrono;
     if(packetsToSend <= 0) return;
 
     while(packetsToSend > 0)
@@ -92,10 +98,10 @@ void sendBurst(std::shared_ptr<PacketCreator>& pc, ull delay, int packetsToSend,
         while(rem--)
         {
             pc->sendHead();
-            std::this_thread::sleep_for(std::chrono::milliseconds(IFG));
+            std::this_thread::sleep_for(milliseconds(IFG));
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+        std::this_thread::sleep_for(milliseconds(delay));
     }
 }
 
@@ -106,31 +112,33 @@ void sendBurst(std::shared_ptr<PacketCreator>& pc, ull delay, int packetsToSend,
 //thread function to send packets
 void createNumBased(std::shared_ptr<PacketCreator> pc, ull number,  ull gap = 0)
 {
+    using namespace std::chrono;
     while(number--)
     {
         int lenRcv = ConfigurationManager::getConfiguration()->getReceivers().size();
         for(int rcvInd=0; rcvInd < lenRcv; rcvInd++)
-        {
             pc->createPacket(rcvInd);
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(gap));
+
+        if(gap > 0)
+            std::this_thread::sleep_for(milliseconds(gap));
     }
 }
 
 /// \param duration time of sending in milliseconds (10^-3) seconds
 void createTimeBased (std::shared_ptr<PacketCreator> pc, ull duration,  ull gap = 0)
 {
-    time_t beginTime = time(NULL);
-    time_t endTime = time(NULL);
+    using namespace std::chrono;
+    time_point<steady_clock> beginTime = steady_clock::now();
+    time_point<steady_clock> endTime = steady_clock::now();
 
-    int lenRcv = ConfigurationManager::getConfiguration()->getReceivers().size();
-    while(endTime - beginTime > duration)
+    int lengthReceived = ConfigurationManager::getConfiguration()->getReceivers().size();
+    while (duration_cast<milliseconds>(endTime- beginTime).count() <= duration)
     {
-        for(int rcvInd=0; rcvInd < lenRcv; rcvInd++)
-            pc->createPacket(rcvInd);
+        for(int receiveIndex=0; receiveIndex < lengthReceived; receiveIndex++)
+            pc->createPacket(receiveIndex);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(gap));
-        endTime = time(NULL);
+        std::this_thread::sleep_for(milliseconds(gap));
+        endTime = steady_clock::now();
     }
 }
 
@@ -154,22 +162,43 @@ int main(int argc, char** argv)
         std::cout<<"No ARGS ARE PASSED\n";
         return 0;
     }
-    
+
     ConfigurationManager::getConfiguration(configPath);
     std::shared_ptr<Configuration> currConfig = ConfigurationManager::getConfiguration();
     std::shared_ptr<StatsManager> sm = StatsManager::getInstance(genID, true);
     PacketSender::getInstance(genID, FIFO_FILE, 0777);
 
-
     std::shared_ptr<PacketCreator> pc = std::make_shared<PacketCreator>();
-
     /// TODO configure main appropriately to run one of the above threads based on configuration parameters
-    /// Flowtype and a new configuration parameter which tells me whether to send time based or flow based
-    std::thread creator(createNumBased,pc,(ull)500,(ull)0);
-    std::thread sender(sendingFunction,pc);
+    /// send according to one of the above threads using flowtype and sending mode
+    /// sending mode is inferred from the numOfPackets and flowTime parameters in the stream configuration
+
+
+    std::thread creator, sender;
     std::thread statWriter(sendStatsFunction, sm);
+    ull gap = currConfig->getInterFrameGap();
+    FlowType flowType = currConfig->getFlowType();
+    if(currConfig->getBcFramesNum() > 0)
+    {
+        int packetNumber = currConfig->getNumberOfPackets();
+        creator = std::thread(createNumBased, pc, packetNumber,0);
+        //TODO add burst delay and burst size from stream configuration
+        if(flowType == BURSTY)
+            sender = std::thread(sendBurst, pc, packetNumber, (ull)20, 30,gap);
+        else
+            sender = std::thread(sendB2B, pc, packetNumber, gap);
+    }
+    else
+    {
+        ull sendTime = currConfig->getLifeTime();
+        creator = std::thread(createTimeBased, pc, sendTime,10);
+        //TODO add burst delay and burst size from stream configuration
+        if(flowType == BURSTY)
+            sender = std::thread(sendTimeBasedBurst, pc, sendTime, (ull)20,30,gap);
+        else
+            sender = std::thread(sendTimeBasedB2B, pc, sendTime, gap);
 
-
+    }
     creator.join();
     sender.join();
     statWriter.join();
